520bc697d0103bc581aaa7b5884454fb6e81f83e
Paul <paulsteyn@gmail.com>
2025-06-17 23:24:22 +0200

    Milestone 1a

---
+++ VSEGenerativeMediaBridge/__init__.py
@@ -0,0 +1,22 @@
+bl_info = {
+    "name": "VSE Generative Media Bridge",
+    "author": "Paul & Joshua",
+    "version": (0, 1, 0),
+    "blender": (4, 0, 0),
+    "location": "VSE > Add > Generative Media",
+    "description": "Bridge between the VSE and external generative tools",
+    "warning": "",
+    "doc_url": "",
+    "category": "VSE",
+}
+
+def register():
+    """Register the addon classes."""
+    pass
+
+def unregister():
+    """Unregister the addon classes."""
+    pass
+
+if __name__ == "__main__":
+    register() 
